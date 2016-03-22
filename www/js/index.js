019c1dab2087df7361d4310f6459552483875a4a
robson <rcitaliano@gmail.com>
2016-03-22 20:39:04 +0100

    added handlebars plugin to create the templates on the page

--- www/js/index.js
+++ www/js/index.js
@@ -35,20 +35,71 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    onDeviceReady: function () {
+        //app.receivedEvent('deviceready');
+
+        //=======================================carousel=======================================
+        var carousel = $("#myCarousel");
+        carousel.carousel("pause").removeData();
+        carousel.carousel(0);
+        var context = {
+            items: [
+                { image: "assets/img/IMG_0383.JPG", title: "titolo1", text: "testo testo testo testo testo testo testo testo testo 1", link: "#", linkText: "link1" }
+                ,{ image: "assets/img/IMG_0607.JPG", title: "titolo2", text: "testo testo testo testo testo testo testo testo testo 2", link: "#", linkText: "link2" }
+                ,{ image: "assets/img/IMG_0709.JPG", title: "titolo3", text: "testo testo testo testo testo testo testo testo testo 3", link: "#", linkText: "link3" }
+                //,{ image: "assets/img/IMG_0607.JPG", title: "titolo4", text: "testo testo testo testo testo testo testo testo testo 4", link: "#", linkText: "" }
+                //,{ image: "assets/img/IMG_0709.JPG", title: "titolo5", text: "testo testo testo testo testo testo testo testo testo 5", link: "#", linkText: "" }
+                //,{ image: "assets/img/IMG_0383.JPG", title: "titolo6", text: "testo testo testo testo testo testo testo testo testo 6", link: "#", linkText: "" }
+                //,{ image: "assets/img/IMG_0383.JPG", title: "titolo7", text: "testo testo testo testo testo testo testo testo testo 7", link: "#", linkText: "" }
+                //,{ image: "assets/img/IMG_0383.JPG", title: "titolo8", text: "testo testo testo testo testo testo testo testo testo 8", link: "#", linkText: "" }
+            ]
+        };
+        var templateIndicators = Handlebars.compile($("#carousel-indicators-template").html());
+        var templateItems = Handlebars.compile($("#carousel-items-template").html());
+        
+        carousel.append(templateItems(context));
+        carousel.append(templateIndicators(context));
+
+        $(".carousel-indicators > li").first().addClass("active");
+
+        $(".carousel-inner > .item").each(function(index, element){
+            if (index == 0)
+                $(this).addClass("active first-slide");
+            else if ($(this).is(':last-child'))
+                $(this).addClass("third-slide");
+            else
+                $(this).addClass("second-slide");
+        });
+        //=======================================carousel end=======================================
+        //=======================================headings=======================================
+        var templateHeadings = Handlebars.compile($("#headings-template").html());
+        $("#headings").append(templateHeadings(context));
+        //=======================================headings end=======================================
+        //=======================================featurette=======================================
+        var templateFeaturette = Handlebars.compile($("#featurette-template").html());
+        $("#headings").append(templateFeaturette(context));
+        //=======================================featurette end=======================================
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
+
+        var i = 0;
+        alert(i++);
         var parentElement = document.getElementById(id);
+        alert(i++);
         var listeningElement = parentElement.querySelector('.listening');
+        alert(i++);
         var receivedElement = parentElement.querySelector('.received');
+        alert(i++);
 
         listeningElement.setAttribute('style', 'display:none;');
+        alert(i++);
         receivedElement.setAttribute('style', 'display:block;');
+        alert(i++);
 
         console.log('Received Event: ' + id);
-        navigator.splashscreen.show();
-        setTimeout(function () { navigator.splashscreen.hide(); }, 3000);
+        alert(i++);
+        //navigator.splashscreen.show();
+        //setTimeout(function () { navigator.splashscreen.hide(); }, 3000);
     }
 };
